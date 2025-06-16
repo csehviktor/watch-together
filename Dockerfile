19ce0acdc2f8380d5eafddced1d9c5ea70da3131
skidoodle <contact@albert.lol>
2025-06-16 03:03:01 +0200

    add docker for production

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM oven/bun:1 AS ui
+WORKDIR /app
+ENV VITE_WS_ENDPOINT=
+COPY ui/ ./ui
+WORKDIR /app/ui
+RUN bun install
+RUN bun run build
+
+
+FROM golang:1.24 AS backend
+WORKDIR /app
+COPY backend/ ./backend
+COPY --from=ui /app/ui/dist ./backend/ui/dist
+WORKDIR /app/backend
+
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o server .
+
+FROM gcr.io/distroless/static-debian12
+WORKDIR /app
+COPY --from=backend /app/backend/server .
+COPY --from=backend /app/backend/ui/dist ./ui/dist
+
+ENTRYPOINT ["/app/server"]
--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/csehviktor/watch-together/routes"
 	"golang.org/x/net/websocket"
@@ -13,6 +15,19 @@ const addr = ":3000"
 func main() {
 	http.Handle("/createroom/", websocket.Handler(routes.HandleCreateRoom))
 	http.Handle("/joinroom/{code}/", websocket.Handler(routes.HandleJoinRoom))
+
+	dist := "ui/dist"
+	fs := http.FileServer(http.Dir(dist))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fp := filepath.Join(dist, r.URL.Path)
+		if stat, err := os.Stat(fp); err == nil && !stat.IsDir() {
+			fs.ServeHTTP(w, r)
+			return
+		}
+
+		http.ServeFile(w, r, filepath.Join(dist, "index.html"))
+	})
 
 	log.Printf("starting socket server on addr %s", addr)
 
